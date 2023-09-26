08ba20c037de1ee48e323abd8bf67391db811a3e
oycoyj2 <oycoyj2@gmail.com>
2023-09-26 10:06:51 +0900

    #7 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 기술
    * 스프링 웹
    *엑츄에이터
    *데브툴즈
    *롬복

---
+++ base-project-board/src/test/java/com/base/projectboard/BaseProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseProjectBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
