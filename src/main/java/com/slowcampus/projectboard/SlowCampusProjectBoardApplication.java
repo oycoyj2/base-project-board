ebd2d8eecfd7f2a0338caf5852771424d77cfa85
oycoyj2 <oycoyj2@gmail.com>
2023-10-04 16:54:33 +0900

    base로 초기화
    꼬인 git 정리하기 - 오프라인에서 작업하던 slow 플젝을 base로 합치는 중

---
+++ src/main/java/com/slowcampus/projectboard/SlowCampusProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.slowcampus.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowCampusProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowCampusProjectBoardApplication.class, args);
+	}
+
+}
