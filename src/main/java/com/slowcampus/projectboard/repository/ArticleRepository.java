e92ee81e3839f9e655e90085e05800a97619a38d
oycoyj2 <oycoyj2@gmail.com>
2023-10-04 19:10:23 +0900

    초기화
    다시 해보자

--- src/main/java/com/slowcampus/projectboard/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.slowcampus.projectboard.repository;
-
-import com.slowcampus.projectboard.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
