51d726747644aaeb06b2dbd9f43447b332e6a858
oycoyj2 <oycoyj2@gmail.com>
2023-10-05 17:08:08 +0900

    Spring Date REST 제외 처리
    무겁고 불필요한 테스트를 @DIsabled로 제외시켰다.
    
    통합테스트라 무거우며 db에도 영향을 준다. 공부에는 도움이 되므로 삭제하진 않고 제외한다.

--- src/main/java/com/base/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/base/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.base.projectboard.repository;
 
 import com.base.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
