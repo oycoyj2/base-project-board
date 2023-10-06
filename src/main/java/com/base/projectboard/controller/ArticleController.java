de0c897df0facddb03eecdbb5574904944efed82
oycoyj2 <oycoyj2@gmail.com>
2023-10-06 15:17:36 +0900

    게시글 상세 페이지 테스트
    완료. article은 나중에 실제 데이터를 넣어줘야 함.

--- src/main/java/com/base/projectboard/controller/ArticleController.java
+++ src/main/java/com/base/projectboard/controller/ArticleController.java
@@ -22,7 +22,7 @@ public class ArticleController {
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, ModelMap map)
     {
-        map.addAttribute("article", null);
+        map.addAttribute("article", "article"); //TODO: 구현할 때 여기에 실제 데이터
         map.addAttribute("articleComments", List.of());
         return "articles/detail";
     }
