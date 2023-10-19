cb7de5c44403559dd4b85386a88caded54c8e34d
oycoyj2 <oycoyj2@gmail.com>
2023-10-19 14:38:35 +0900

    SecurityConfig 구현
    But 전에 쓰던 코드를 베껴온 것이기 때문에 나중에 base에 맞게 바꾸어주어야 한다.

--- src/main/java/com/base/projectboard/config/SecurityConfig.java
+++ src/main/java/com/base/projectboard/config/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.base.projectboard.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -14,11 +19,40 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .formLogin(withDefaults());
+                .formLogin(
+                        login -> login
+                                .loginPage("/articles/login")
+                                .defaultSuccessUrl("/")
+                                .usernameParameter("email")
+                                .failureUrl("/articles/login/error")
+                );
+
+        http.
+                logout(
+                        logout -> logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/articles/logout"))
+                                .logoutSuccessUrl("/")
+                );
+
+
+        http
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
+                                .requestMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                );
 
         return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
